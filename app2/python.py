9679e8ffda02e94e4a37a7b4697c8064851312b0
Christine <christinekhekama99@gmail.com>
2023-09-27 17:30:46 +0300

    question2 in app2

---
+++ app2/python.py
@@ -0,0 +1,24 @@
+def solution(S):
+    count = 0
+    n = len(S)
+    
+# Check if the first character is the same as the last character
+    if S[0] == S[-1]:
+        count += 1
+    
+# Convert the string to a list for efficient splicing
+    S = list(S)
+    
+    for i in range(n-1):
+# Move the first character to the end
+        S.append(S.pop(0))
+        
+# Check if the first character is the same as the last character
+        if S[0] == S[-1]:
+            count += 1
+    
+    return count
+
+print(solution("abbaa"))  
+print(solution("aaaa"))  
+print(solution("abab")) 
---
+++ app2/script.js
@@ -0,0 +1,28 @@
+function solution(S) {
+    let count = 0;
+    const n = S.length;
+    
+// Check if the first character is the same as the last character
+    if (S[0] === S[n - 1]) {
+        count++;
+    }
+    
+    S = S.split('');
+    
+    for (let i = 0; i < n - 1; i++) {
+// Move the first character to the end
+        S.push(S.shift());
+        
+// Check if the first character is the same as the last character
+        if (S[0] === S[n - 1]) {
+            count++;
+        }
+    }
+    
+    return count;
+}
+
+
+console.log(solution("abbaa")); 
+console.log(solution("aaaa")); 
+console.log(solution("abab")); 
